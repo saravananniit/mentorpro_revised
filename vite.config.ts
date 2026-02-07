9e14ce439939f432a7e90365076b2ae0e1087381
Saravanan P <saravanan.1.p@niit.com>
2026-02-07 14:00:44 +0530

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/mentorpro_revised/",
   plugins: [react()],
-});
+  base: '/mentorpro_revised/',
+  build: {
+    outDir: 'dist',
+  },
+})
